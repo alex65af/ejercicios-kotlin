17af006b13ef0b45604605038976fcb563c86f65
Alex Flores_escritorio <alexander65.af@gmail.com>
2023-10-15 22:53:14 -0400

    Primer commit kotlin

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,28 @@
+fun main() {
+    println("Hello World!")
+
+    //Variable
+
+    var myString = "Esto es una cade de texto"
+    myString = "Aqui camibio el valor de la cadena de texto"
+    println(myString)
+
+    var myString2: String = "Esta es otra cadena de texto"
+    println(myString2)
+
+    var myInt = 7
+    myInt = myInt + 4
+    println(myInt)
+    println(myInt - 1)
+
+    var myInt2: Int = 4
+    println(myInt2)
+
+    var myDouble = 6.5
+    println(myDouble)
+
+    myDouble = 6.0 //Para cambiar el valor de un double debemos pasar con un valor decimal
+    println(myDouble)
+
+
+}
